b7b92da8b644fb9b21243ca41c51799786daf9a8
Camila-Leon <camilaecommerce12@gmail.com>
2021-12-09 11:59:07 -0300

    desafio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import NavBar from "./components/NavBar/NavBar";
+import ItemCount from "./components/itemCount";
 import ItemListContainer from "./components/ItemListContainer/itemListContainer";
 import "./styles/styles.css";
+import Contador from "./components/itemCount";
 
 function App() {
   return (
       <center>
       <NavBar />
+      <Contador />
       <ItemListContainer greeting='Tenemos variedad de productos' />
-      <h1>
-        soy un texto
-      </h1>
+        <h1>
+          soy un texto
+        </h1>
       </center>
 
   )
