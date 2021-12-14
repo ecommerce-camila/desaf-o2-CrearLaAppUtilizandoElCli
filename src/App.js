e347a077ffa7e5d80a037ccda608fed6110a6ab7
Camila-Leon <camilaecommerce12@gmail.com>
2021-12-14 13:02:33 -0300

    desafio

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         </h1>
       </center>
 
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Item/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = (props) => {
+    return (
+        <>
+            <h1>{props.greeting}ItemListContainer</h1>
+        </>
+    );
+};
+
+export default ItemListContainer;
