1eb51a8dd18483f50919f56f58ac7d6e57bcc618
Camila-Leon <camilaecommerce12@gmail.com>
2021-12-21 19:10:50 -0300

    desafio

--- src/components/Item/ItemDetail.jsx
+++ src/components/Item/ItemDetail.jsx
@@ -1,11 +1,14 @@
 const ItemDetail = ({ item }) => {
     console.log("El item", item);
     return (
-      <div className="flex bg-black">
-        <h3>{item.title}</h3>
-        <p> {item.description}</p>
-        <img src={`${item.image}`} alt={`${item.image}`}></img>
-      </div>
+        <div>
+          <div className="flex bg-black">
+              <h3>{item.title}</h3>
+              <p> {item.description}</p>
+              <img src={`${item.image}`} alt={`${item.image}`} />
+          </div>
+          <ItemCount stock={item.stock} />
+        </div>    
     );
   };
   export default ItemDetail;
