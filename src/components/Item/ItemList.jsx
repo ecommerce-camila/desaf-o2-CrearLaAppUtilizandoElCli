caf807b65711d64e857bf69d9d54905858d0c37b
Camila-Leon <camilaecommerce12@gmail.com>
2021-12-21 11:18:57 -0300

    desafio

--- src/components/Item/ItemList.jsx
+++ src/components/Item/ItemList.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import Item from './Item';
 
-const ItemList = ({ items, products }) => {
-    console.log("Los products", products);
-    return (
-      <div>
-        {items?.map((item) => (
-        <Item item={item} key={item.id} />
-        ))}
-        {products.map((product) => {
-          return (
-            <Item
-              key={product.id}
-              description={product.descripcion}
-              category={product.categoria}
-              image={product.image}
-              price={product.price}
-              stock={product.stock}
-              title={product.title}
-              id={product.id}
-            />
-          );
-        })}
-      </div>
-    );
-  };
-  export default ItemList;
+const ItemList = ({ products }) => {
+  console.log("Los products", products);
+  return (
+    <div>
+      {products.map((product) => {
+        return (
+          <Item
+            key={product.id}
+            description={product.descripcion}
+            category={product.categoria}
+            image={product.image}
+            price={product.price}
+            stock={product.stock}
+            title={product.title}
+            id={product.id}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default ItemList;
